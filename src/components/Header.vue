631c2f9692c428aa0cb1fa44028061ace77376b0
Maxime Gravouil <maxgravo@wkmfr7571087.global.publicisgroupe.net>
2019-10-15 16:37:32 +0200

    First commit

---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <router-link class="navbar-item" to="/">HENRI POTIER</router-link>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <router-link to="/products" class="navbar-item">Products</router-link>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link
+                class="button is-info badge has-badge-rounded has-badge-danger"
+                [attr.data-badge]="order.count"
+                to="/cart"
+              >
+                <span class="icon">
+                  <i class="fas fa-shopping-cart"></i>
+                </span>
+                <strong>View Order</strong>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {}
+</script>
